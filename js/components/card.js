304af06a226dacd407950af5fa7cff7449959ccf
Cécile <cecilephan@outlook.com>
2025-09-19 10:36:13 +0200

    :lipstick: fix style homepage

--- js/components/card.js
+++ js/components/card.js
@@ -1,31 +1,31 @@
 class Card extends HTMLElement {
   connectedCallback() {
     // Récupérer les attributs
-    const imageUrl = this.getAttribute('image-url') || 'https://wallpaperaccess.com/full/4990824.png';
-    const title1 = this.getAttribute('title1') || 'Lorem ipsum dolor sit amet.';
-    const title2 = this.getAttribute('title2') || 'Lorem ipsum dolor sit amet.';
-    const price = this.getAttribute('price') || '45,00€';
-    const imageClass = this.getAttribute('image-class') || 'object-cover w-full';
-    const cardClass = this.getAttribute('card-class') || '';
-    const fullWidth = this.hasAttribute('full-width');
-    
+    const imageUrl =
+      this.getAttribute('image-url') ||
+      'https://wallpaperaccess.com/full/4990824.png'
+    const title1 = this.getAttribute('title1') || 'Lorem ipsum dolor sit amet.'
+    const title2 = this.getAttribute('title2') || 'Lorem ipsum dolor sit amet.'
+    const price = this.getAttribute('price') || '45,00€'
+    const fullWidth = this.hasAttribute('full-width')
+
     this.innerHTML = `
-      <div class="card flex flex-col w-fit p-4 ${cardClass}">
-        <div class="card-img-container p-8 ${fullWidth ? 'full-width-container' : ''}">
-          <img src="${imageUrl}" alt="" class="${imageClass}" />
+      <div class="card flex flex-col gap-20">
+        <div class="card-container-img ${fullWidth ? 'full-width' : ''}">
+          <img src="${imageUrl}" alt="" class="h-full ${
+      fullWidth ? 'w-full' : ''
+    }" />
         </div>
-        <div class="card-content-row mt-4">
-          <div class="card-text-container flex flex-col">
-            <h5>${desc1}</h5>
-            <h5>${desc2}</h5>
+        <div class="product-infos flex flex-col gap-20">
+          <div class="flex flex-col gap-10 h4">
+            <p>${title1}</p>
+            <p>${title2}</p>
           </div>
-          <div class="card-price-container ml-8">
-            <p class="body-m font-bold">${price}</p>
-          </div>
+          <p class="body-m font-bold">${price}</p>
         </div>
       </div>
-    `;
+    `
   }
 }
 
-customElements.define('app-card', Card);
+customElements.define('app-card', Card)
